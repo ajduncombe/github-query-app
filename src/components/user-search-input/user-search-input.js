7575400cbb4c6852da7124583b5972bce03bc633
James Humphreys <james@binaryjim.co.uk>
2021-10-17 16:51:45 +0100

    #1: Refactor of UserSearchInput component logic

--- src/components/user-search-input/user-search-input.js
+++ src/components/user-search-input/user-search-input.js
@@ -1,60 +1,144 @@
-import { React, useState } from 'react';
-import { PropTypes } from 'prop-types';
-import { TextField, Button, InputAdornment } from '@mui/material';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { React, useState } from "react";
+import { PropTypes } from "prop-types";
+import { TextField, Button, InputAdornment } from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-export function UserSearchInput({ username, onSubmit }) {
-    const [value, setValue] = useState('');
+/*
+export function UserSearchInput({
+  submitting,
+  onSubmit,
+  inputValue,
+  onInputValueChange,
+}) {
+  const handleChange = (e) => {
+    onInputValueChange(e.target.value);
+  };
 
-    const handleChange = (e) => {
-        setValue(e.target.value);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit();
+  };
 
-    const handleSubmit = (e) => {
-        alert('Submitted!');
-        e.preventDefault();
-    }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: "flex", alignItems: "center" }}
+    >
+      <TextField
+        required
+        id="outlined-required"
+        label="GitHub username"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+        value={inputValue}
+        onChange={handleChange}
+        disabled={submitting}
+      />
 
-    if (onSubmit) {
-        ;
-    } else {
-        const submitButton = <Button style={{marginLeft:20}}
-            variant="contained"
-            size="large"
-            disabled={!value}
-            startIcon={<ArrowForwardIosIcon />}
-            type="submit"
-        >Search</Button>;
-    }
+      {submitting ? (
+        <Button
+          style={{ marginLeft: 20 }}
+          variant="contained"
+          size="large"
+          disabled
+          startIcon={<ArrowForwardIosIcon />}
+          type="submit"
+        >
+          Submitting...
+        </Button>
+      ) : (
+        <Button
+          style={{ marginLeft: 20 }}
+          variant="contained"
+          size="large"
+          disabled={!inputValue}
+          startIcon={<ArrowForwardIosIcon />}
+          type="submit"
+        >
+          Search
+        </Button>
+      )}
+    </form>
+  );
+}
+*/
 
-    return (
-        <form onSubmit={handleSubmit} style={{display:"flex", alignItems:"center"}}>
-            <TextField
-                required
-                id="outlined-required"
-                label="GitHub username"
-                InputProps={{
-                    startAdornment: (
-                    <InputAdornment position="start">
-                        <AccountCircle />
-                    </InputAdornment>
-                    ),
-                }}
-                value={username}
-                onChange={handleChange}
-            />
+export function UserSearchInput({ onSubmit }) {
+  const [inputValue, setInputValue] = useState("");
+  const [submitting, setSubmitting] = useState(false);
 
-            {submitButton}
-        </form>
-    );
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    await onSubmit(inputValue);
+
+    setSubmitting(false);
+    setInputValue("");
+  };
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+  };
+
+  return (
+    <form
+      onSubmit={handleOnSubmit}
+      style={{ display: "flex", alignItems: "center" }}
+    >
+      <TextField
+        required
+        id="outlined-required"
+        label="GitHub username"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+        value={inputValue}
+        onChange={handleChange}
+        disabled={submitting}
+      />
+
+      {submitting ? (
+        <Button
+          style={{ marginLeft: 20 }}
+          variant="contained"
+          size="large"
+          disabled
+          startIcon={<ArrowForwardIosIcon />}
+          type="submit"
+        >
+          Submitting...
+        </Button>
+      ) : (
+        <Button
+          style={{ marginLeft: 20 }}
+          variant="contained"
+          size="large"
+          disabled={!inputValue}
+          startIcon={<ArrowForwardIosIcon />}
+          type="submit"
+        >
+          Search
+        </Button>
+      )}
+    </form>
+  );
 }
 
 UserSearchInput.propTypes = {
-    username: PropTypes.string,
-    onSubmit: PropTypes.func,
-}
+  username: PropTypes.string,
+  onSubmit: PropTypes.func,
+};
 
-UserSearchInput.defaultProps = {
-
-}
+UserSearchInput.defaultProps = {};
--- src/stories/user-search-input.stories.js
+++ src/stories/user-search-input.stories.js
@@ -1,19 +1,48 @@
-import React from 'react'
-import { UserSearchInput } from '../components/user-search-input'
+import React, { useState } from "react";
+import { UserSearchInput } from "../components/user-search-input";
 
 const config = {
-    title: 'App/Components/User Search Input',
-    component: UserSearchInput,
+  title: "App/Components/User Search Input",
+  component: UserSearchInput,
+};
+
+/*
+function Default(args) {
+  const [value, setValue] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleOnSubmit = () => {
+    setSubmitting(true);
+
+    setTimeout(() => {
+      console.log("The form value is:", value);
+      setSubmitting(false);
+    }, [2000]);
+  };
+
+  return (
+    <UserSearchInput
+      onSubmit={handleOnSubmit}
+      submitting={submitting}
+      inputValue={value}
+      onInputValueChange={(value) => setValue(value)}
+    />
+  );
+}
+*/
+
+function Default(args) {
+  const handleOnSubmit = (value) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("The form value is:", value);
+        resolve();
+      }, [2000]);
+    });
+  };
+
+  return <UserSearchInput onSubmit={handleOnSubmit} />;
 }
 
-function Template(args) {
-    return <UserSearchInput {...args} />
-}
-
-const Default = Template.bind({})
-Default.args = {
-    onSubmit: (value) => console.log(value)
-}
-
-export default config
-export { Default }
+export default config;
+export { Default };
